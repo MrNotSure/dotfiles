f8ec8105dce5c26bda45268894fd2e92c2b179e9
Emanuel Buchholz <emanuel.buchholz@deutschebahn.com>
2023-02-01 16:00:17 +0100

    Task: Start adaption to make the two shells zsh and bash compatible and reuse as much as possible in both.
    Add: kubernetes topic
    Refactor: rename aliases.zsh to aliases.shell file in order to indicate
    compatibility between both shells
    Refactor: Loading settings in *.symlink files
    Delete: Node folder because I don't know what it is for.

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,12 +1,9 @@
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+
+
+# Add bash specific aliases here:
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias speak='a=$(fc -ln -0); b="  process is done."; spd-say "$a$b"; unset a; unset b'
-
-alias dc='cd'
